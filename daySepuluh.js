cc4b01eb9501e860239304b3201f7538382139a9
ABINUBLII <nubliabi97@gmail.com>
2024-01-15 19:47:37 +0700

    Object dan Object constructor

---
+++ daySepuluh.js
@@ -0,0 +1,14 @@
+//object
+
+let biodata = {
+  nama: "abi nubli farandi",
+  kelas: 10,
+  jurusan: "ipa",
+};
+
+// cara lain penulisan object (object constructor)
+let murid = new Object();
+murid.nama = "abi";
+murid.umur = 18;
+
+console.log(murid);
