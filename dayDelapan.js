d72be8550fe4f3977233d7c65f592eba2f7048f9
ABINUBLII <nubliabi97@gmail.com>
2024-01-17 11:06:16 +0700

    looping Materials

--- dayDelapan.js
+++ dayDelapan.js
@@ -1,51 +1,74 @@
-// for (let data = 0; data < 10; data++) {
-//   console.log("halo");
-// }
-
-//buat looping 1 s.d 10 kemudian tulis ganjil sesuai angka
-// for (let i = 0; i <= 10; i++) {
-//   if (i)
-// }
-
-// for (data = 1; data <= 10; data++){
-//     if()
-// }
-
 // for (let i = 1; i <= 10; i++) {
-//     if (i % 2 === 0) {
-//       console.log("Genap=", i);
-//     } else {
-//       console.log("Ganjil=", i);
-//     }
-//   }
-
-// buat looping for
-// ada array [2,2,6,10]
-// output hasil dari array.
-let data = [2, 2, 6, 10];
-let result = 0;
-// (let i = 0; i < data.length; i++) data.length untuk mmenghitung berapa banyak array
-for (let i = 0; i < data.length; i++) {
-  result += data[i];
-}
-console.log(result);
-console.log(data.length); // buat ngecek aja berapa banyak array
-
-// buat perkalian 2,5,10 dari array
-// output hasil dari perkalian tsb
-let data2 = [2, 5, 10];
-let hasil2 = 0;
-
-for (let i = 0; i < data2.length; i++) {
-  hasil2 *= data2[i];
-}
-console.log(hasil2);
-
-// let data2 = [2, 5, 10];
-// let hasil2 = 0;
-
-// for (let i = 0; i < data2.length; i++) {
-//   hasil2 *= data2[i]; // ini sama aja dengan
-//   hasil2 = data2[i] * data2[i]; // ini
+//   console.log("hi binarian", i, "x");
 // }
-// console.log(hasil2);
+
+//challange buat looping 1-10 dan outputnya :
+// 1 ganjil
+// // 2 genap
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i, "genap");
+//   } else {
+//     console.log(i, "ganjil");
+//   }
+// }
+
+// challange
+// let array = [2,5,6,10] lalu dijumlahkan
+// let result nya berapa ?
+
+// let data = [2, 5, 6, 10];
+// let result = 0;
+
+// for (let indexData = 0; indexData < data.length; indexData++) {
+//   console.log(indexData);
+//   result += data[indexData];
+//   // console.log(indexData[i]);
+// }
+// console.log(`hasilnya adalah ${result}`);
+
+// faris hasan
+
+let data = [2, 5, 6, 10];
+console.log(data);
+let result = 0;
+
+for (let i = 0; i <= 3; i++) {
+  result += data[i];
+  console.log(data[i]);
+}
+
+console.log(` hasil dari si hasan ${result}`);
+
+// // challange
+// // let array = [] lalu multiplys numbernya
+// // let result nya berapa ?
+
+// let multNumbs = [2, 2, 5];
+// let resultMult = 0;
+
+// for (let i = 0; i < multNumbs.length; i++) {
+//   resultMult = multNumbs[i] * multNumbs[i];
+// }
+// console.log(resultMult);
+// console.log(multNumbs.length);
+
+// let numberItem = [10, 2];
+// let resultDivide = 0;
+
+// for (let i = 0; i < numberItem.length; i++) {
+//   resultDivide = numberItem[i] / numberItem[i];
+//   // console.log(numberItem[i]);
+// }
+// console.log(resultDivide);
+
+// while loop
+for (let i = 0; i < 5; i++) {
+  console.log(`hai siswa binarian`);
+}
+
+let index = 0;
+while (index < 5) {
+  console.log("hai binarian, `ini while loop`");
+  index++;
+}
